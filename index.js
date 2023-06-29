b4bfbf7e9b0319a06f207d621ea220fc598ac8cb
lekimberly17 <lekimberly509@gmail.com>
2023-06-29 11:48:52 -0700

    workshop 1

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ const app = express();
 const { Joke } = require('./db');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+ 
+    const tags = req.query.tags;
+    const content = req.query.content;
+ 
+    let jokes = await Joke.findAll();
+    if (tags) {
+      jokes = jokes.filter(j => j.tags.includes(tags));
+    }
+    if (content) {
+      jokes = jokes.filter(j => j.joke.includes(content));
+    }
     res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error)
   }
-});
+ });
 
-// we export the app, not listening in here, so that we can run tests
+// We export the app, not listening in here, so that we can run tests
 module.exports = app;
